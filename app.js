26278d47c59500fa23bd455e138b27f1a0d716e4
Paquito81 <95262020+Paquito81@users.noreply.github.com>
2022-09-05 20:51:30 -0700

    add

--- app.js
+++ app.js
@@ -1,13 +1,5 @@
-let cash = 100 
-let price = 200
-let difference = cash - price 
- 
-if (cash > price) {
-    console.log(`you paid extra - here's ${difference} dollars change`)
-}
-else if (cash === price) {
-    console.log("you paid the exact amount, have a nice day!")
-} 
-else {
-    console.log(`not enough money - you still owe ${difference * -1} dollars`)
-}
+let subscribed = true
+let loggedIn = true
+
+let str = subscribed && loggedIn ? 'show the video' : 'hide the video'
+console.log(str);
