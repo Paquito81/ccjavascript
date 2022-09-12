424e5da3f90dbca1c446c91cc38fd2b4f3fe1a91
Paquito81 <95262020+Paquito81@users.noreply.github.com>
2022-09-12 05:22:37 -0700

    string

--- app.js
+++ app.js
@@ -1,5 +1,41 @@
-let subscribed = true
-let loggedIn = true
+//DRY dont repeat yourself
 
-let str = subscribed && loggedIn ? 'show the video' : 'hide the video'
-console.log(str);
+// let count = 1;
+// while (count <= 100) {
+//     console.log(count)
+//     count = count + 1;
+// }
+
+/** Write a for-loop that loops 1 to 20
+ * If the number id divisible by 3, print "frontend"
+ * If the number is divisible by 5, print "Simplified"
+ * If the number is divisible by 3 and 5, print "Frontend Simplified"
+ * If the number is *not* divisible by 3 or 5 , print the number 
+ * 
+ * @example1 
+ * 
+ * 1 -> 1 
+ * 2 -> 2 
+ * 3-> "Frontend"
+ * 4 -> 4
+ * 5 -> "Simplified"
+ * ...
+ * 15 -> "Frontend Simplified"
+ * ...
+ * 20 -> "Simplified"
+ */
+
+for (let i = 1; i <=20; ++i) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log(`${i} -> Frontend Simplified`)
+    }
+    else if (i % 3 === 0) {
+        console.log(`${i} -> Frontend`)
+    }
+    else if (i % 5 === 0) {
+        console.log(`${i} -> Simplified`)
+    }
+    else {
+        console.log(`${i} -> ${i}`)
+    }
+}
---
+++ index.js
@@ -0,0 +1,18 @@
+/** Print out every character from the string: 
+ * "Frontend Simplified"
+ * 
+ * @example
+ * 'F'
+ * 'r'
+ * 'o'
+ * ...
+ * 'e
+ * 'd'
+ */
+
+const str = "Frontend Simplified"
+
+for (let i = 0; i < str.length; ++i) {
+    console.log(str[i])
+
+}
