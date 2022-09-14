9561815644e45de511ef14e25377cc8dbf93cf66
Paquito81 <95262020+Paquito81@users.noreply.github.com>
2022-09-14 05:36:13 -0700

    add

---
+++ functions.js
@@ -0,0 +1,10 @@
+// DRY - Don't repeat yourself
+
+//function definition
+function welcomePersonToFES(name) {
+    console.log(`Welcome to FES, ${name}`)
+}
+//Call the function
+welcomePersonToFES('Hector');
+welcomePersonToFES('David');
+welcomePersonToFES('Mitri');
