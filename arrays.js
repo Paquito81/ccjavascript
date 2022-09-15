180c990b3a38f31ed6e5519bbee7a5f86f4cecb1
Paquito81 <95262020+Paquito81@users.noreply.github.com>
2022-09-15 00:41:01 -0700

    add

--- arrays.js
+++ arrays.js
@@ -1,7 +1,22 @@
 let arr =[20, 30, 40, 50, 100]
 
-//Friwt element of array:
-console.log(arr[0])
+let newArr = arr.filter((element) => {
+    console.log(element)
+    return true;
+})
 
-//Last element of array:
-console.log(arr[arr.length - 1])
+console.log(newArr)
+
+//Friwt element of array:
+// console.log(arr[0])
+
+// //Last element of array:
+// console.log(arr[arr.length - 1])
+
+// //Add element onto end of array
+// arr.push(200)
+
+// arr.filter(() => {})
+
+// console.log(arr);
+
